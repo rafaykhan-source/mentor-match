0cff83d21eaacc63d8a4858e6f643d084a9af8e4
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2024-11-19 23:07:50 -0500

    feat: add file output strategies

--- src/mentor_match/datawriter.py
+++ src/mentor_match/datawriter.py
@@ -1,16 +1,66 @@
 """This module is responsible for writing data to disk."""
 
 import csv
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 from mentor_match.models import Group
 
 
 @dataclass
-class DataWriter:
-    """This class is responsible for writing the data."""
+class FileOutputStrategy(ABC):
+    """This interface is for data writing."""
 
-    def write_entries(self, groups: list[Group], file_path: str) -> None:
+    @abstractmethod
+    def write_groups(self, groups: list[Group], file_path: str) -> None:
+        """Writes the groups to output.
+
+        Args:
+            groups (list[Group]): The groups to write to output.
+            file_path (str): The path for the output file.
+        """
+
+
+@dataclass
+class CSVOutputStrategy(FileOutputStrategy):
+    """This class is for writing data to csv."""
+
+    def write_groups(self, groups: list[Group], file_path: str) -> None:
+        """Writes the groups to output.
+
+        Args:
+            groups (list[Group]): The groups to write to output.
+            file_path (str): The path for the output file.
+        """
+        with open(file_path, "w", newline="") as csv_file:
+            fieldnames = [
+                "mentor",
+                "mentees",
+                "meeting_time",
+            ]
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for g in groups:
+                writer.writerow(
+                    {
+                        "mentor": g.mentor.__repr__(),
+                        "mentees": g.mentees,
+                        "meeting_time": g.meeting_time,
+                    },
+                )
+
+
+class SimpleCSVOutputStrategy(CSVOutputStrategy):
+    """This class is for writing data to csv in a simple format."""
+
+    def write_groups(self, groups: list[Group], file_path: str) -> None:
+        """Writes the groups to output.
+
+        Args:
+            groups (list[Group]): The groups to write to output.
+            file_path (str): The path for the output file.
+        """
         with open(file_path, "w", newline="") as csv_file:
             fieldnames = [
                 "mentor",
@@ -28,3 +78,12 @@ class DataWriter:
                         "meeting_time": g.meeting_time,
                     },
                 )
+
+
+@dataclass
+class DataWriter:
+    """This class is responsible for writing the data."""
+
+    def write_entries(self, groups: list[Group], file_path: str) -> None:
+        s = CSVOutputStrategy()
+        s.write_groups(groups, file_path)
