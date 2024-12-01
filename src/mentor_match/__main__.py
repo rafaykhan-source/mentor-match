9ab55a7aedc06942e034089bdf1a66c138efb2cb
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2024-11-30 22:19:40 -0500

    feat: add argparse

--- src/mentor_match/__main__.py
+++ src/mentor_match/__main__.py
@@ -1,8 +1,20 @@
+import argparse
+
 from mentor_match.dataserver import DataServer
 from mentor_match.mentee_strategies import SmallestGroupStrategy
 from mentor_match.mentor_strategies import FrequencyStrategy
 from mentor_match.models import Group
 from mentor_match.rematch import RematchCleaner, RematchEngine
+
+
+def get_args() -> argparse.Namespace:
+    """Returns program arguments."""
+    parser = argparse.ArgumentParser(
+        prog="mentor-match",
+        description="Forms mentorship groups between students and mentors.",
+    )
+    parser.add_argument("operation", help="match, rematch, or clean")
+    return parser.parse_args()
 
 
 def initial_match() -> None:
@@ -20,13 +32,13 @@ def initial_match() -> None:
     print("\n".join([str(len(g.mentees)) for g in groups]))
 
 
-def _clean() -> None:
+def clean() -> None:
     c = RematchCleaner()
     c.update_rematchees()
 
 
 def rematch() -> None:
-    _clean()
+    clean()
 
     s = DataServer()
     rematchees = s.read_people("data/updates/rematchees.json")
@@ -37,7 +49,15 @@ def rematch() -> None:
 
 
 def main() -> None:
-    initial_match()
+    args = get_args()
+
+    match args.operation:
+        case "match":
+            initial_match()
+        case "rematch":
+            rematch()
+        case "clean":
+            clean()
 
 
 main()
